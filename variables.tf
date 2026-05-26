9147f14d5c44f013cbca4eb869b5f5a2321d8eed
harry.sarshogh <harry.sarshogh@gmail.com>
2026-05-26 23:13:58 +0100

    feat: implement tf-atom-route-aws module

--- variables.tf
+++ variables.tf
@@ -1,7 +1,38 @@
-# -----------------------------------------------------------------------------
-# Module-Specific Variables
-#
-# Note: Standard labeling variables (enabled, namespace, tenant, environment,
-# stage, name, delimiter, attributes, tags, label_order, etc.) are provided
-# by context.tf via the tf-label module.
-# -----------------------------------------------------------------------------
+variable "route_table_id" {
+  description = "ID of the route table"
+  type        = string
+  validation {
+    condition     = length(var.route_table_id) > 0
+    error_message = "route_table_id must not be empty."
+  }
+}
+
+variable "destination_cidr_block" {
+  description = "Destination CIDR block for the route"
+  type        = string
+  default     = "0.0.0.0/0"
+}
+
+variable "gateway_id" {
+  description = "ID of the internet gateway"
+  type        = string
+  default     = null
+}
+
+variable "nat_gateway_id" {
+  description = "ID of the NAT gateway"
+  type        = string
+  default     = null
+}
+
+variable "transit_gateway_id" {
+  description = "ID of the transit gateway"
+  type        = string
+  default     = null
+}
+
+variable "vpc_peering_connection_id" {
+  description = "ID of the VPC peering connection"
+  type        = string
+  default     = null
+}
