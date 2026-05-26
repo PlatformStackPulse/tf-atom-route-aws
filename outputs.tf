8e6bc0c9bf70177b8363ec2f96f1f34bb198db06
PE Stack Pulse <harry.sarshogh@gmail.com>
2026-05-26 23:25:35 +0100

    feat: implement tf-atom-route-aws module (#1)

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,14 @@
 output "enabled" {
-  description = "Whether the module is enabled."
+  description = "Whether the module is enabled"
   value       = local.enabled
+}
+
+output "route_table_id" {
+  description = "Route table ID the route belongs to"
+  value       = try(aws_route.this[0].route_table_id, null)
+}
+
+output "destination_cidr_block" {
+  description = "Destination CIDR of the route"
+  value       = try(aws_route.this[0].destination_cidr_block, null)
 }
