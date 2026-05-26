f7c5db58d2778a261825633eeda435492da95718
harry.sarshogh <harry.sarshogh@gmail.com>
2026-05-26 23:13:58 +0100

    feat: implement tf-atom-route-aws module

---
+++ main.tf
@@ -0,0 +1,10 @@
+resource "aws_route" "this" {
+  count = module.this.enabled ? 1 : 0
+
+  route_table_id            = var.route_table_id
+  destination_cidr_block    = var.destination_cidr_block
+  gateway_id                = var.gateway_id
+  nat_gateway_id            = var.nat_gateway_id
+  transit_gateway_id        = var.transit_gateway_id
+  vpc_peering_connection_id = var.vpc_peering_connection_id
+}
